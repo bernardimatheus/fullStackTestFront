e666221e5b5e60e862038e6f966189b983128655
Motomco <motomco@Mac.local>
2019-10-29 17:01:27 -0300

    Add table and calling API

--- src/pages/ListClient/index.js
+++ src/pages/ListClient/index.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import ReactTable from 'react-table';
+import './react-table.css';
+
+import api from '../../services/api';
+
+import { Container, Content } from './styles';
 
 export default function ListClient() {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    api.get(`${process.env.REACT_APP_DEV_API_URL}/clients`).then(res => {
+      setClients(res.data.data);
+    });
+  }, [clients]);
+
+  const columns = [
+    {
+      Header: 'ID',
+      accessor: 'id',
+    },
+    {
+      Header: 'Name',
+      accessor: 'name', // String-based value accessors!
+    },
+    {
+      Header: 'CPF',
+      accessor: 'cpf',
+    },
+    {
+      Header: 'E-mail',
+      accessor: 'email',
+    },
+    {
+      Header: 'Telefone',
+      accessor: 'phone',
+    },
+    {
+      Header: 'Ações',
+    },
+  ];
+
   return (
     <Container>
-      <h1>List</h1>
+      <Content>
+        <ReactTable
+          data={clients}
+          columns={columns}
+          defaultPageSize={10}
+          nextText="Próxima"
+          previousText="Anterior"
+          className="-striped -highlight"
+        />
+      </Content>
     </Container>
   );
 }
--- src/pages/ListClient/styles.js
+++ src/pages/ListClient/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   height: 100%;
   background: rgb(114, 109, 187);
   background: linear-gradient(
@@ -10,4 +14,11 @@ export const Container = styled.div`
     rgba(102, 136, 195, 1) 51%,
     rgba(22, 136, 159, 1) 100%
   );
+`;
+
+export const Content = styled.div`
+  max-width: 900px;
+  margin: 20px;
+  padding: 15px;
+  width: 100%;
 `;
